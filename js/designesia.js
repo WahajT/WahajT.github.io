93656d6baeae8e763059bfb19d29954cd19c507a
WahajT <wahajtahir01@gmail.com>
2024-10-27 23:05:05 +0500

    optimized

--- js/designesia.js
+++ js/designesia.js
@@ -698,25 +698,6 @@
             }
         });
 
-        jQuery("#carousel-blog-4").owlCarousel({
-            center: false,
-            items: 2,
-            loop: false,
-            margin: 25,
-            responsive: {
-                1000: {
-                    items: 2
-                },
-                600: {
-                    items: 2
-                },
-                0: {
-                    items: 1
-                }
-            }
-        });
-
-
         jQuery("#owl-logo").owlCarousel({
             center: false,
             items: 6,
